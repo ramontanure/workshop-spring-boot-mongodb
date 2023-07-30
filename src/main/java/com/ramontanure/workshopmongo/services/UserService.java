3be44cb6855cd3692fac1d4631736af364c476ba
Ramon Tanure <ramon_tanure_carvalho@hotmail.com>
2023-07-30 17:17:52 -0300

    Deleção de usuario com DELETE

--- src/main/java/com/ramontanure/workshopmongo/services/UserService.java
+++ src/main/java/com/ramontanure/workshopmongo/services/UserService.java
@@ -30,8 +30,12 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDto(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-			
 	}
 }
