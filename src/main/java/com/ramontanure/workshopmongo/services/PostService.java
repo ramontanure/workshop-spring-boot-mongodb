4b1e8b65a18d0d7b14c115e928a1a7937fc13606
Ramon Tanure <ramon_tanure_carvalho@hotmail.com>
2023-07-30 18:00:45 -0300

    Obtendo post por id

---
+++ src/main/java/com/ramontanure/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.ramontanure.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramontanure.workshopmongo.domain.Post;
+import com.ramontanure.workshopmongo.repository.PostRepository;
+import com.ramontanure.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
